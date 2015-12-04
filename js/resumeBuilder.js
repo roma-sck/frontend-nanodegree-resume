1eec9cdf664ffebd862e9892152c1204fc6b8821
roma.sck <roma.sck@gmail.com>
2015-12-04 02:13:37 +0200

    first changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var name = "Roman Fomenko";
+ var role = "Android Developer";
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").append(formattedName);
+ $("#header").append(formattedRole);
