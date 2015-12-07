bd35aa9d0438006a64080ca64bace4f0b5c99d10
roma.sck <roma.sck@gmail.com>
2015-12-08 00:46:10 +0200

    add Education, Progects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,7 +6,7 @@ My resume.
     "name": "Roman Fomenko",
     "role": "Android Developer",
     "contacts": {
-        "mobile": "223-322-223-322  :-)",
+        "mobile": "223-322-223-322",
         "email": "roma.sck@gmail.com",
         "github": "roma-sck@github.com",
         "location": "Dnipropetrovsk"
@@ -60,26 +60,7 @@ if (bio.skills.length > 0)
             $("#skills").append(formattedSkills);
     }
 }
-var education = {
-    "schools": [
-    {
-        "name": "National Mining University",
-        "city": "Dnipropetrovsk",
-        "degree": "Specialist",
-        "majors":["Mining Engineer"],
-        "dates": "2004-2009",
-        "url": "www.nmu.org.ua"
-    }
-    ],
-    "onlineCourses": [
-    {
-        "title": "Android development",
-        "school": "Online school from e-Legion and Google",
-        "dates": 2015,
-        "url": "http://learndroid.e-legion.ru/"
-    }
-    ]
-};
+
 
 var work = {
     "jobs": [
@@ -98,9 +79,9 @@ var work = {
     ]
 };
 
-function displayWork()
+function displayWork() 
 {
-    for (job in work.jobs)
+    for (job in work.jobs) 
     {
         $('#workExperience').append(HTMLworkStart);
         var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
@@ -125,5 +106,106 @@ displayWork();
 // });
 
 
+// /*function changes name "jOhN dOe" to "John DOE"*/
+// function inName(name)
+// {
+//     name = name.trim().split(" ");
+//     console.log(name);
+//     name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+//     name[1] = name[1].toUpperCase();
+//     return name[0] + " " + name[1];
+// }
+
+//$("#main").append(internationalizeButton);
+
+var education = {
+    "school":
+    {
+        "name": "National Mining University",
+        "city": "Dnipropetrovsk",
+        "degree": "Specialist",
+        "majors":["Mining Engineer"],
+        "dates": "2004-2009",
+        "url": "www.nmu.org.ua"
+    },
+    "courses": [
+    {
+        "title": "Fundamentals of the Java programming language",
+        "school": "Corporate Training Center \"SeeTech\"",
+        "dates": "2014 ",
+        "url": "http://seetech.com.ua"
+    },
+    {
+        "title": "Android development",
+        "school": "Online school from e-Legion and Google",
+        "dates": "2015 ",
+        "url": "http://learndroid.e-legion.ru/"
+    },
+    {
+        "title": "JavaScript Crash Course",
+        "school": "Udacity",
+        "dates": "2015 ",
+        "url": "http://www.udacity.com/course/ud804"
+    }
+    ]
+};
+
+function displayEducation()
+{
+    $('#education').append(HTMLschoolStart);
+    var formattedName = HTMLschoolName.replace('%data%', education.school.name);
+    $(".education-entry:last").append(formattedName);
+    var formattedDates = HTMLschoolDates.replace('%data%', education.school.dates);
+    $(".education-entry:last").append(formattedDates);
+    var formattedLocation = HTMLschoolLocation.replace('%data%', education.school.city);
+    $(".education-entry:last").append(formattedLocation);
+    var formattedMajor = HTMLschoolMajor.replace('%data%', education.school.majors);
+    $(".education-entry:last").append(formattedMajor);
+
+  
+    for (course in education.courses)
+    {
+        var formattedTitle = HTMLonlineTitle.replace('%data%', education.courses[course].title);
+        $(".education-entry:last").append(formattedTitle);
+        var formattedSchool = HTMLonlineSchool.replace('%data%', education.courses[course].school);
+        $(".education-entry:last").append(formattedSchool);
+        var formattedDates = HTMLonlineDates.replace('%data%', education.courses[course].dates);
+        $(".education-entry:last").append(formattedDates);
+        var formattedURL = HTMLonlineURL.replace('%data%', education.courses[course].url);
+        $(".education-entry:last").append(formattedURL);
+    }
+}
+
+displayEducation();
 
 
+
+var projects = {
+    "projects": [
+    {
+        "title": "Censor News Reader",
+        "dates": "2015",
+        "description": "Simple Android news reader from http://censor.net.ua. Source code : https://github.com/roma-sck/Censor-News-Reader",
+        "image" : "https://raw.githubusercontent.com/roma-sck/frontend-nanodegree-resume/master/proj_screenshot.jpg"
+    }
+    ]
+};
+
+projects.display = function()
+    {
+        for (project in projects.projects)
+        {
+            $('#projects').append(HTMLprojectStart);
+            var formattedTitle = HTMLprojectTitle.replace('%data%', projects.projects[project].title)
+            $(".project-entry:last").append(formattedTitle);
+            var formattedDates = HTMLprojectDates.replace('%data%', projects.projects[project].dates)
+            $(".project-entry:last").append(formattedDates);
+            var formattedDescription = HTMLprojectDescription.replace('%data%', projects.projects[project].description)
+            $(".project-entry:last").append(formattedDescription);
+			var formattedImage = HTMLprojectImage.replace('%data%', projects.projects[project].image)
+            $(".project-entry:last").append(formattedImage);
+        }
+
+    }
+
+projects.display();
